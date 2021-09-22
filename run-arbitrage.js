09c8bcde79fa474ec7d36207719a989ddead87ac
Constantin <constantin.chtanko@etu.formation-aftec.com>
2021-09-22 17:13:41 +0200

    block receive

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -6,3 +6,13 @@ const web3 = new Web3(
     //keep yout infura secret
     new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
     );
+//receive new blocks
+//reevaluate oppurtunities everytime in the new block
+//block will emit data
+web3.eth.subscribe('newBlockHeaders')
+    .on('data', async block => {
+        console.log(`New block received. Block # ${block.number}`);
+    })
+    .on('error', error => {console.log(error);//if error    
+    });
+    
